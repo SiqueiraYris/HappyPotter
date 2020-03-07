e9c4a14011337eabeace96a81fb34fccd408041d
Siqueira <siqueira@c0532.local>
2020-03-07 19:27:55 -0300

    Adding a default xcode project

---
+++ HappyPotter/Application/Coordinator.swift
@@ -0,0 +1,22 @@
+//
+//  Coordinator.swift
+//  HappyApp
+//
+//  Created by Siqueira on 06/03/20.
+//  Copyright © 2020 Siqueira. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator: AnyObject {
+
+    func start()
+
+}
+
+extension Coordinator {
+
+    func start() {}
+
+}
---
+++ HappyPotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HappyPotter
+//
+//  Created by Siqueira on 07/03/20.
+//  Copyright © 2020 Siqueira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
