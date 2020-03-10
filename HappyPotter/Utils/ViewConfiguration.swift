3324b714f077c32156ea3d2a88b0cd0bb3789a79
Yris Siqueira <siqueirayris@hotmail.com>
2020-03-10 14:47:24 -0300

    Adding dynamic type

---
+++ HappyPotter/Utils/ViewConfiguration.swift
@@ -0,0 +1,26 @@
+//
+//  ViewConfiguration.swift
+//  HappyPotter
+//
+//  Created by Siqueira on 10/03/20.
+//  Copyright © 2020 Siqueira. All rights reserved.
+//
+
+import Foundation
+
+public protocol ViewConfiguration: AnyObject {
+    func setupConstraints()
+    func buildViewHierarchy()
+    func configureViews()
+    func setupViewConfiguration()
+}
+
+extension ViewConfiguration {
+    public func setupViewConfiguration() {
+        buildViewHierarchy()
+        setupConstraints()
+        configureViews()
+    }
+
+    public func configureViews() {}
+}
